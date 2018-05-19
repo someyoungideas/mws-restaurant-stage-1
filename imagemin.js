87c7ad7a929418c65f262ca62948c2069ac8e664
someyoungideas <someyoungideas@gmail.com>
2018-05-19 00:33:21 -0500

    creating imagemin npm scripts and imagemin.js to optimize, resize, and create webp images

---
+++ imagemin.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const imagemin = require('imagemin')
+const imageminJpegRecompress = require('imagemin-jpeg-recompress')
+const imageminWebp = require('imagemin-webp')
+const sharp = require('sharp')
+const mkdirp = require('mkdirp')
+
+mkdirp(`${__dirname}/build/images`, (err) => {
+  if (err) 
+    console.error(err)
+
+  fs.readdir(`${__dirname}/img`, (err, files) => {
+    files.forEach(file => {
+      const sizes = [800, 400]
+      const resizePromises = sizes.map(size => {
+        const fileName = `${file.slice(0, file.lastIndexOf('.'))}_${size}${file.slice(file.lastIndexOf('.'), file.length)}`
+
+        return sharp(`${__dirname}/img/${file}`)
+              .resize(size)
+              .max()
+              .toFile(`${__dirname}/build/images/${fileName}`)
+      })
+
+      return Promise.all(resizePromises).then(_ => {
+        return imagemin(['img/*.jpg'], 'build/images', {
+            use: [
+              imageminJpegRecompress()
+            ]
+        })
+      }).then(_ => {
+        return imagemin(['img/*.jpg'], 'build/images', {
+            use: [
+              imageminWebp({quality: 50})
+            ]
+        })
+      }).catch(console.error)
+    })
+  })
+})
